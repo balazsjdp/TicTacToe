748601cfee3b6b10817951aab15a00d1882ac275
balazsjdp <balazsjdp@gmail.com>
2020-02-29 19:33:09 +0100

    first commit

---
+++ ticTacToe/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace ticTacToe
+{
+    partial class main_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.A1 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // A1
+            // 
+            this.A1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.A1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A1.Font = new System.Drawing.Font("Segoe UI Light", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.A1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.A1.Location = new System.Drawing.Point(12, 12);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(100, 100);
+            this.A1.TabIndex = 0;
+            this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.Button_Clicked);
+            // 
+            // B1
+            // 
+            this.B1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.B1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B1.Font = new System.Drawing.Font("Segoe UI Light", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.B1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.B1.Location = new System.Drawing.Point(118, 12);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(100, 100);
+            this.B1.TabIndex = 1;
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.Button_Clicked);
+            // 
+            // C1
+            // 
+            this.C1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.C1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.C1.Font = new System.Drawing.Font("Segoe UI Light", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.C1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.C1.Location = new System.Drawing.Point(224, 12);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(100, 100);
+            this.C1.TabIndex = 2;
+            this.C1.UseVisualStyleBackColor = true;
+            this.C1.Click += new System.EventHandler(this.Button_Clicked);
+            // 
+            // A2
+            // 
+            this.A2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.A2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A2.Font = new System.Drawing.Font("Segoe UI Light", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.A2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.A2.Location = new System.Drawing.Point(12, 118);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(100, 100);
+            this.A2.TabIndex = 3;
+            this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.Button_Clicked);
+            // 
+            // B2
+            // 
+            this.B2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.B2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B2.Font = new System.Drawing.Font("Segoe UI Light", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.B2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.B2.Location = new System.Drawing.Point(118, 118);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(100, 100);
+            this.B2.TabIndex = 4;
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.Button_Clicked);
+            // 
+            // C2
+            // 
+            this.C2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.C2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.C2.Font = new System.Drawing.Font("Segoe UI Light", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.C2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.C2.Location = new System.Drawing.Point(224, 118);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(100, 100);
+            this.C2.TabIndex = 5;
+            this.C2.UseVisualStyleBackColor = true;
+            this.C2.Click += new System.EventHandler(this.Button_Clicked);
+            // 
+            // A3
+            // 
+            this.A3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.A3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A3.Font = new System.Drawing.Font("Segoe UI Light", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.A3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.A3.Location = new System.Drawing.Point(12, 224);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(100, 100);
+            this.A3.TabIndex = 6;
+            this.A3.UseVisualStyleBackColor = true;
+            this.A3.Click += new System.EventHandler(this.Button_Clicked);
+            // 
+            // B3
+            // 
+            this.B3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.B3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B3.Font = new System.Drawing.Font("Segoe UI Light", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.B3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.B3.Location = new System.Drawing.Point(118, 224);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(100, 100);
+            this.B3.TabIndex = 7;
+            this.B3.UseVisualStyleBackColor = true;
+            this.B3.Click += new System.EventHandler(this.Button_Clicked);
+            // 
+            // C3
+            // 
+            this.C3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.C3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.C3.Font = new System.Drawing.Font("Segoe UI Light", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.C3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.C3.Location = new System.Drawing.Point(222, 224);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(100, 100);
+            this.C3.TabIndex = 8;
+            this.C3.UseVisualStyleBackColor = true;
+            this.C3.Click += new System.EventHandler(this.Button_Clicked);
+            // 
+            // main_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(334, 341);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "main_form";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "TicTacToe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button C3;
+    }
+}
+
---
+++ ticTacToe/TicTacToeApp.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ticTacToe
+{
+    public partial class main_form : Form
+    {
+        private bool turn = true; // true -> player ; false -> computer 
+
+
+        public main_form()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            b.Text = "X";
+            b.Enabled = false;
+
+            string winner = isThereAWinner();
+            if(winner != null)
+            {
+                MessageBox.Show(winner + " is the winner!");
+                startNewGame();
+                return;
+            }
+
+            if (isGameOver() == true)
+            {
+                MessageBox.Show("DRAW! Game over!");
+                startNewGame();
+            }
+            else
+            {
+                computersTurn((Button)sender);
+                winner = isThereAWinner();
+                if (winner != null)
+                {
+                    MessageBox.Show(winner + " is the winner!");
+                    startNewGame();
+                    return;
+                }
+            }
+        }
+
+
+        private void computersTurn(Button playerMove)
+        {
+            string playerMoveColumn = playerMove.Name.Substring(0, 1);
+            int playerMoveRow = Convert.ToInt32(playerMove.Name.Substring(1, 1));
+
+            isPlayerAboutToWin(playerMoveColumn, playerMoveRow);
+           
+        }
+
+        private void randomMove()
+        {
+            Console.WriteLine("Random move");
+            int emptyCells = 0;
+            Button[] buttons = new Button[Controls.Count];
+            foreach (Control control in Controls)
+            {
+                if (control is Button && control.Enabled == true)
+                {
+                    buttons[emptyCells] = (Button)control;
+                    emptyCells++;
+                }
+            }
+
+            Random rnd = new Random();
+            int nextTurnIndex = rnd.Next(0, emptyCells);
+
+            buttons[nextTurnIndex].Text = "O";
+            buttons[nextTurnIndex].Enabled = false;
+        }
+
+        private void isPlayerAboutToWin(string column,int row)
+        {
+            bool moveDone = false;
+            string previousColumn;
+            string nextColumn;
+            int previousRow;
+            int nextRow;
+
+            string diagonalNextColumn;
+            string diagonalPreviousColumn;
+            int diagonalNextRow;
+            int diagonalPreviousRow;
+
+            switch (column)
+            {
+                case "A":
+                    previousColumn = null;
+                    nextColumn = "B";
+                    break;
+                case "B":
+                    previousColumn = "A";
+                    nextColumn = "C";
+                    break;
+                case "C":
+                    previousColumn = "B";
+                    nextColumn = null;
+                    break;
+                default:
+                    previousColumn = null;
+                    nextColumn = null;
+                    break;
+            }
+            switch (row)
+            {
+                case 1:
+                    previousRow = 999;
+                    nextRow = 2;
+                    break;
+                case 2:
+                    previousRow = 1;
+                    nextRow = 3;
+                    break;
+                case 3:
+                    previousRow = 2;
+                    nextRow = 999;
+                    break;
+                default:
+                    previousRow = 999;
+                    nextRow = 999;
+                    break;
+            }
+
+            // Case player is about to win : has two colums or two rows next to each other filled with X
+            List<string> dangerousButtons = new List<string>();
+            string currentMove = column + row.ToString();
+
+            // Horizontal 
+            if(previousColumn != null) dangerousButtons.Add(previousColumn + row.ToString());
+            if(nextColumn != null) dangerousButtons.Add(nextColumn + row.ToString());
+            // Vertical
+            if(previousRow != 999) dangerousButtons.Add(column + previousRow.ToString());
+            if(nextRow != 999) dangerousButtons.Add(column + nextRow.ToString());
+            // Diagonal
+            if (previousColumn != null && nextColumn != null && previousRow != 999 && nextRow != 999)
+            {
+                dangerousButtons.Add(previousColumn + (row - 1).ToString());
+                dangerousButtons.Add(previousColumn + (row + 1).ToString());
+                dangerousButtons.Add(nextColumn + (row - 1).ToString());
+                dangerousButtons.Add(nextColumn + (row + 1).ToString());
+            }
+           
+
+            foreach (string button in dangerousButtons)
+            {
+                if (Controls[button].Text == "X")
+                {
+                    //  The player wins with the next move - > defense needed
+                    // Check if a move is needed vertically or horizontally
+                    if(column == button.Substring(0, 1))
+                    {
+                        Console.WriteLine("SAME COLUMN!");
+                        // Same column - > move vertially
+                        if(Convert.ToInt32(button.Substring(1,1)) == 1)
+                        {
+                            if(Controls[column + "3"].Enabled == true)
+                            {
+                                Controls[column + "3"].Text = "O";
+                                Controls[column + "3"].Enabled = false;
+                                moveDone = true;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            if(Controls[column + "1"].Enabled == true)
+                            {
+                                Controls[column + "1"].Text = "O";
+                                Controls[column + "1"].Enabled = false;
+                                moveDone = true;
+                                break;
+                            }
+                        }
+                    }
+                    else if(row == Convert.ToInt32(button.Substring(1, 1)))
+                    {
+                        Console.WriteLine("SAME ROW!");
+                        if (button.Substring(0, 1) == "A")
+                        {
+                            if(Controls["C" + row].Enabled == true)
+                            {
+                                Controls["C" + row].Text = "O";
+                                Controls["C" + row].Enabled = false;
+                                moveDone = true;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            if(Controls["A" + row].Enabled == true)
+                            {
+                                Controls["A" + row].Text = "O";
+                                Controls["A" + row].Enabled = false;
+                                moveDone = true;
+                                break;
+                            }
+                        }
+                    }
+
+                }
+            }
+
+            if (!moveDone)
+            {
+                randomMove();
+            }
+
+        }
+       
+
+        private string isThereAWinner()
+        {
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled))
+                return A1.Text;
+            if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+                return B1.Text;
+            if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+                return C1.Text;
+            if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+                return A1.Text;
+            if ((C1.Text == B2.Text) && (B2.Text == A3.Text) && (!C1.Enabled))
+                return C1.Text;
+
+            if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+                return A1.Text;
+            if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+                return A2.Text;
+            if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
+                return A3.Text;
+
+
+            return null;
+        }
+
+
+        private void startNewGame()
+        {
+            turn = true;
+            foreach (Control control in Controls)
+            {
+                if (control is Button)
+                {
+                    control.Text = "";
+                    control.Enabled = true;
+                }
+            }
+        }
+
+        private bool isGameOver()
+        {
+            foreach(Control control in Controls)
+            {
+                if(control is Button)
+                {
+                    if (control.Text == "") return false;
+                }
+            }
+            return true;
+        }
+    }
+}
